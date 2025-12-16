e178578fa6455c36a6447a371174d2c8c86b3ecb
peter munyambu <petermunyambu1000@gmail.com>
2025-12-16 03:25:26 +0300

    correct pages

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Enable the App Router
+  experimental: {
+    appDir: true,
+  },
+  // Optional: Add any other Next.js configuration options here
+}
+
+module.exports = nextConfig
